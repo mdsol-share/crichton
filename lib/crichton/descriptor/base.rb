dc220076bd0927e03e31257985b597e2f5de879b
Mark W. Foster <mfoster@mdsol.com>
2013-04-14 21:46:29 -0700

    Refactored to proper package structure and simpler Base class

---
+++ lib/crichton/descriptor/base.rb
@@ -0,0 +1,69 @@
+module Crichton
+  module Descriptor
+    # Manage base functionality for all descriptors that encapsulate a section of a resource descriptor document.
+    #
+    # Adds a private class method <tt>descriptor_reader</tt> to descriptor classes to simplify generating
+    # class specific accessors encapsulating properties in an underlying descriptor document. 
+    #
+    # The following macro tags add the corresponding type to the generated documentation:
+    #
+    #  - array_reader
+    #  - hash_reader
+    #  - object_reader
+    #  - string_reader
+    #
+    # @example
+    #   class Profile < base
+    #     @!macro string_reader
+    #     descriptor_reader: id
+    #   end
+    class Base
+      # @private
+      EXCLUDED_VARIABLES = %w(@descriptors @descriptor_document @resource_descriptor).map(&:to_sym)
+
+      # @private
+      # See lib/crichton.rb for documentation if re-usable macro definitions.
+      def self.descriptor_reader(name)
+        method = name.to_s
+        define_method(method) do
+          descriptor_document[method]
+        end
+      end
+      private_class_method :descriptor_reader
+
+      ##
+      # Constructs a new instance of BaseDocumentDescriptor.
+      #
+      # Subclasses MUST call <tt>super</tt> in their constructors.
+      #
+      # @param [Hash] resource_descriptor The parent resource descriptor instance.                                                              # 
+      # @param [Hash] descriptor_document The section of the descriptor document representing this instance.
+      def initialize(resource_descriptor, descriptor_document)
+        @resource_descriptor = resource_descriptor
+        @descriptor_document = descriptor_document && descriptor_document.dup || {}
+        @descriptors = {}
+      end
+  
+      ##
+      # The underlying descriptor document.
+      #
+      # @return [Hash] The descriptor document.
+      attr_reader :descriptor_document
+  
+      ##
+      # The parent resource descriptor.
+      #
+      # @return [Hash] The resource descriptor.
+      attr_reader :resource_descriptor
+  
+      # @private
+      # Overrides inspect to remove the descriptor document for readability
+      def inspect
+        ivars = (instance_variables - EXCLUDED_VARIABLES).map do |ivar|
+          "#{ivar}=#{instance_variable_get(ivar).inspect}"
+        end
+        '#<%s:0x%s %s>' % [self.class.name, self.hash.to_s(16), ivars.join(", ")]
+      end
+    end
+  end
+end
