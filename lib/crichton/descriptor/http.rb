5a768518a6475205f71ed6b86c7cbe776cb00a31
Mark W. Foster <mfoster@mdsol.com>
2013-04-14 21:49:16 -0700

    Changed base class functionality of Http descriptor and changed package structure

---
+++ lib/crichton/descriptor/http.rb
@@ -0,0 +1,24 @@
+module Crichton
+  module Descriptor
+    # Manages HTTP-protocol transition descriptors.
+    class Http < Base 
+      # @!macro array_reader
+      descriptor_reader :content_types
+
+      # @!macro array_reader
+      descriptor_reader :headers
+
+      # @!macro string_reader
+      descriptor_reader :method
+
+      # @!macro hash_reader
+      descriptor_reader :slt
+
+      # @!macro hash_reader
+      descriptor_reader :status_codes
+
+      # @!macro string_reader
+      descriptor_reader :uri
+    end
+  end
+end
