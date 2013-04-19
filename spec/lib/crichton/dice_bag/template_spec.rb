14f99c2dc6409371d278f0e13bbb109f786164f8
Mark W. Foster <mfoster@mdsol.com>
2013-04-19 07:59:50 -0700

    fix c&p errors and DRY spec

--- spec/lib/crichton/dice_bag/template_spec.rb
+++ spec/lib/crichton/dice_bag/template_spec.rb
@@ -4,15 +4,15 @@ module Crichton
   module DiceBag
     describe Template do
       describe '#tempates_location' do
-        it 'returns the config_directory' do
+        it 'returns the location that templates are generated for a project' do
           Crichton.stub(:config_directory).and_return('config_directory')
-          Template.new.templates_location.should == 'config_directory'
+          subject.templates_location.should == 'config_directory'
         end
       end
   
-      describe '#tempates_location' do
-        it 'returns the config_directory' do
-          Template.new.templates.first.should =~ /crichton.yml.dice$/
+      describe '#tempates' do
+        it 'returns an array of templates' do
+          subject.templates.first.should =~ /crichton.yml.dice$/
         end
       end
     end
