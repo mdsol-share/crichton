415dbe520a4b39c87f077c445e5ee711a567bbb1
Mark W. Foster <mfoster@mdsol.com>
2013-05-10 11:44:30 -0700

    Implemented state transition functionality to determine if a transition is available for particular options.

--- lib/crichton/descriptor/state_transition.rb
+++ lib/crichton/descriptor/state_transition.rb
@@ -6,12 +6,13 @@ module Crichton
     #
     class StateTransition < Base
       ## 
-      # @!attribute [r] conditions
       # The condition options that indicate a transition should be included in a response that represents the
       # associated state of a resource.
       #
       # @return [Array] The conditions.
-     descriptor_reader :conditions
+      def conditions
+        @descriptors[:conditions] ||= (descriptor_document['conditions'] || [])
+      end
 
       ## 
       # @!attribute [r] next
@@ -21,9 +22,32 @@ module Crichton
       # state machine diagrams associated with resources.
       #
       # @return [Array] The next state(s).
-     def next
-       @descriptors[:next] ||= (descriptor_document['next'] || [])
-     end
+      def next
+        @descriptors[:next] ||= (descriptor_document['next'] || [])
+      end
+      
+      ##
+      # Whether the transition is available for the specified options.
+      #
+      # @param [Hash] options Conditional options.
+      # @option options [Hash, String, Symbol] :conditions The conditions to validate against the transition conditions.
+      # @return [Boolean] <tt>true</tt> if available, <tt>false</tt> otherwise.
+      def available?(options = nil)
+        return true if conditions.empty?
+        
+        option_conditions = Array.wrap(options[:conditions] || {})
+        return false if option_conditions.empty?
+
+        conditions.any? do |condition|
+          option_conditions.any? do |option_condition|
+            if condition.is_a?(Hash) && option_condition.is_a?(Hash)
+              option_condition.keys.any? { |k| condition[k.to_s] == option_condition[k].to_s } 
+            elsif condition.is_a?(String)
+              condition == option_condition.to_s
+            end
+          end
+        end
+      end
     end
   end
 end
