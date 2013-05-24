dcb7d2293df4091f1ccd3157861c12f884870508
Mark W. Foster <mfoster@mdsol.com>
2013-05-24 16:02:01 -0700

    Fix order dependency in spec.

--- spec/lib/crichton/representor/serializer_spec.rb
+++ spec/lib/crichton/representor/serializer_spec.rb
@@ -4,54 +4,46 @@ require 'crichton/representor/serializer'
 module Crichton
   module Representor
     describe Serializer do
+      def create_media_type_serializer(serializer = nil)
+        serializer ||= :MediaTypeSerializer
+        Crichton::Representor.send(:remove_const, serializer) if Representor.const_defined?(serializer)
+        clear_serializers
+        
+        eval("class #{serializer} < Crichton::Representor::Serializer; end")
+      end
+
       def clear_serializers
         Serializer.instance_variable_set('@registered_serializers', {})
       end
-      
-      def clear_media_type_serializer(serializer = nil)
-        Crichton::Representor.send(:remove_const, serializer || :MediaTypeSerializer)
-        clear_serializers
-      end
-      
-      def create_media_type_serializer(serializer = nil)
-        eval("class #{serializer || 'MediaTypeSerializer'} < Crichton::Representor::Serializer; end")
-      end
-      
-      def wrap(example)
-        create_media_type_serializer
-        example.run
-        clear_media_type_serializer
-      end
-      
+
       let(:object) do
         Class.new { include Representor }.new
       end
-      
+
       context 'when subclassed' do
         context 'with serializer subclasses with well-formed names' do
-          around do |example|
-            wrap(example)
-          end
-
           it 'auto registers sublclassed serializers' do
+            create_media_type_serializer
             Serializer.registered_serializers[:media_type].should == MediaTypeSerializer
           end
         end
-        
+
         context 'with alternate media types defined for the serializer' do
-          it 'auto registers alternate media types' do
+          before do
             eval("class MediaTypeSerializer < Crichton::Representor::Serializer; alternate_media_types " <<
               ":alt_media_type, 'other_alt_media_type'; end")
+          end
+          
+          it 'auto registers alternate media types as symbols' do
             Serializer.registered_serializers[:alt_media_type].should == MediaTypeSerializer
+          end
+
+          it 'auto registers alternate media types as strings' do
             Serializer.registered_serializers[:other_alt_media_type].should == MediaTypeSerializer
           end
         end
-  
+
         context 'with serializer subclasses with mal-formed names' do
-          after do
-            clear_media_type_serializer(@serializer)
-          end
-          
           it 'raises an error when the name does not end in Serializer' do
             @serializer = :MediaTypeSerializers
             expect { create_media_type_serializer(@serializer) }.to raise_error(Crichton::Representor::Error,
@@ -62,10 +54,10 @@ module Crichton
 
       describe '.build' do
         context 'with existing subclasses' do
-          around do |example|
-            wrap(example)
+          before do
+            create_media_type_serializer
           end
-          
+
           it 'builds serializer instances associated with a media type' do
             Serializer.build(:media_type, object).should be_instance_of(MediaTypeSerializer)
           end
@@ -75,30 +67,24 @@ module Crichton
               /^The object .* is not a Crichton::Representor.$/)
           end
         end
-        
+
         it 'raises an error if the type does not have a registered serializer' do
           expect { Serializer.build(:some_media_type, object) }.to raise_error(Crichton::Representor::Error,
             /^No representor serializer is registered that corresponds to the type 'some_media_type'.$/)
         end
       end
-      
+
       describe '.registered_serializers' do
-        around do |example|
-          clear_serializers
-          example.run
-          clear_serializers
-        end
-        
-        it 'returns an empty hash if no serializers are registered' do
-          Serializer.registered_serializers.should == {}
-        end
-        
-        context 'with existing subclasses with well-formed names' do
-          around do |example|
-            wrap(example)
+        context 'without any registered serializers' do
+          it 'returns an empty hash if no serializers are registered' do
+            clear_serializers
+            Serializer.registered_serializers.should == {}
           end
-          
+        end
+
+        context 'with existing subclasses with well-formed names' do
           it 'returns a hash of registered serializer classes' do
+            create_media_type_serializer
             Serializer.registered_serializers[:media_type].should == MediaTypeSerializer
           end
         end
