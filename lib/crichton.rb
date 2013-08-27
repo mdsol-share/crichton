94acee044a917f5669388b88b370dc3537f33da6
Phil Servedio <pservedio@mdsol.com>
2013-08-27 15:59:22 -0700

    Put in rake requirement for crichton.rb

--- lib/crichton.rb
+++ lib/crichton.rb
@@ -7,6 +7,8 @@ require 'crichton/dice_bag/template'
 require 'crichton/representor'
 require 'crichton/alps/deserialization'
 
+require 'crichton/rake_lint' if defined?(Rails)
+
 module Crichton
   ##
   # Logger
