c6ca176862950ea087858c21fb26f6f2681a6f02
Chaim Solomon <csolomon@mdsol.com>
2013-09-09 20:21:38 +0300

    Addressed comment - moved file rewind to after block

--- spec/lib/crichton/alps/deserialization_spec.rb
+++ spec/lib/crichton/alps/deserialization_spec.rb
@@ -4,15 +4,18 @@ module Crichton
   module ALPS 
     describe Deserialization do
       describe '.new' do
+        after do
+          # Rewind file so the next user can read it
+          alps_xml_string.rewind
+        end
+
         it 'deserializes XML data' do
           deserializer = Deserialization.new(alps_xml_string)
           deserializer.to_hash.keys.should == ["text", "doc", "ext", "links", "descriptors"]
         end
 
         it 'deserializes JSON data' do
-          alps_xml_string.rewind
           xmldeserializer = Crichton::ALPS::Deserialization.new(alps_xml_string.read)
-          alps_xml_string.rewind
           deserializer = Deserialization.new("{\"alps\": #{xmldeserializer.to_json}}")
           deserializer.to_hash.keys.should == ["texts", "doc", "ext", "links", "descriptors"]
         end
