eb8026d946ce9970017ad190fc95f8dd3b89c66b
abrutski <abrutski@mdsol.com>
2013-09-12 20:42:14 -0400

    fixing responder

--- lib/crichton/core_ext/action_controller/responder.rb
+++ lib/crichton/core_ext/action_controller/responder.rb
@@ -1,17 +1,25 @@
 module ActionController
   class Responder
     def navigation_behavior(error)
+      if get?
+        if resource.is_a?(Crichton::Representor)
+          api_behavior(error)
+        else
+          raise error
+        end
+      elsif has_errors? && default_action
+        render :action => default_action
+      else
+        redirect_to navigation_location
+      end
+    end
+
+    def resourceful?
+      return resource.respond_to?("to_#{format}") unless resource.is_a?(Crichton::Representor)
       if request.env['HTTP_ACCEPT'] && request.env['HTTP_ACCEPT'].include?('text/html')
         options.merge!(semantics: :styled_microdata)
       end
-
-      if get?
-        display resource
-      elsif post?
-        display resource, status: :created, location: api_location
-      else
-        head :no_content
-      end
+      resource.is_a?(Crichton::Representor)
     end
   end
 end
