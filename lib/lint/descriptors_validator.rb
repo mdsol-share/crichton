9e5d1d47d1076c0fff69eca60188995a735c217d
Phil Servedio <pservedio@mdsol.com>
2013-09-20 16:52:06 -0700

    Descriptor changes and specs as well as refactoring

--- lib/lint/descriptors_validator.rb
+++ lib/lint/descriptors_validator.rb
@@ -1,60 +1,140 @@
 require 'lint/base_validator'
 
+
 module Lint
   class DescriptorsValidator < BaseValidator
+    TOP_LEVEL = 0
     VALID_MIME_TYPES = %w(html)
-    #Look at Descriptor::Profile::TRANSITION_TYPES in profile.rb
-    VALID_DESCRIPTOR_TRANSITIONS = %w(safe unsafe idempotent)
+    VALID_DESCRIPTOR_TYPES = Crichton::Descriptor::Profile::SEMANTIC_TYPES +
+      Crichton::Descriptor::Profile::TRANSITION_TYPES
 
     def validate
+      check_descriptor_graph
 
-      check_for_secondary_descriptor_states
+      compare_with_state_resources
 
       check_transition_equivalence
     end
 
     private
-    def check_for_secondary_descriptor_states
-      resource_descriptor.descriptors.each do |descriptor|
-        options = {resource: resource_descriptor.name, filename: filename}
+    def check_descriptor_graph
+      check_descriptor_level(@resource_descriptor.descriptors, {}, TOP_LEVEL)
+    end
 
-        add_error('catastrophic.no_descriptors', options) if descriptor.descriptors.empty?
+    def check_descriptor_level(descriptors, options, level)
+      descriptors.each do |descriptor|
+#        options = {resource: concat_descriptor_names(descriptor.name, options), filename: filename}
+        options = {resource: descriptor.name, filename: filename}
+        descriptor_properties_check(descriptor, options, level)
+        check_descriptor_level(descriptor.descriptors, options, level+1) if descriptor.descriptors
+      end
+    end
+
+    # TODO: Fix or remove
+    def concat_descriptor_names(name, options)
+      options[:resource] ? options[:resource] << ":" << name : name
+    end
+
+    # do checks for the properties common to both semantic and transition type descriptors
+    def descriptor_properties_check(descriptor, options, level)
+      common_properties_check(descriptor, options, level)
+      semantic_properties_check(descriptor, options, level) if descriptor.semantic?
+      transition_properties_check(descriptor, options, level) if descriptor.transition?
+    end
+
+    def common_properties_check(descriptor, options, level)
+      #20, should have a valid mime type for doc property
+      if descriptor.doc
+        unless valid_media_type?(descriptor.doc)
+          add_error('descriptors.doc_media_type_invalid', options.merge({media_type: descriptor.doc.keys.last}))
+        end
+      else
         #19, check for missing doc property
-        add_warning('descriptors.property_missing', options.merge({prop: 'doc'})) unless descriptor.doc
+        add_error('descriptors.property_missing', options.merge({prop: 'doc'}))
+      end
 
-        #20, should only have a valid mime type for doc
-        if descriptor.doc
-          unless valid_media_type(descriptor.doc)
-            add_error('descriptors.doc_media_type_invalid',options.merge({media_type: descriptor.doc.keys.last}))
-          end
+      #22
+      if descriptor.type
+        unless VALID_DESCRIPTOR_TYPES.include?(descriptor.type)
+          add_error('descriptors.type_invalid', options.merge({type_prop: descriptor.type}))
         end
+      else
         #21 should have a type property
-        add_error('descriptors.property_missing', options.merge({prop: 'type'})) unless descriptor.type
-        #22
-        if descriptor.type
-          unless descriptor.type == 'semantic'
-            add_error('descriptors.type_invalid',options.merge({type_prop: descriptor.type}))
-          end
-        end
+        add_error('descriptors.property_missing', options.merge({prop: 'type'}))
+      end
 
-        #22 should have a link property
-        add_error('descriptors.property_missing', options.merge({prop: 'link'})) unless descriptor.type_link
+      if level == TOP_LEVEL
+        add_error('catastrophic.no_descriptors', options) if descriptor.descriptors.empty?
 
         #23 should have a valid link property
-        if descriptor.type_link
-          add_error('descriptors.link_invalid', options) unless valid_link_property(descriptor.type_link)
+        if descriptor.links.empty?
+          #22 should have a link property
+          add_warning('descriptors.property_missing', options.merge({prop: 'link'}))
+        else
+          unless valid_link_property?(descriptor.link['self'])
+            add_error('descriptors.link_invalid', options.merge({link: descriptor.link.keys.first}))
+          end
         end
       end
     end
 
     #TODO: For decorator class
     # A media type can be of text, which in case is a simple string, or a hash with specific keys with a value
-    def valid_media_type(doc)
-      doc.is_a?(String) || (doc.is_a?(Hash) && VALID_MIME_TYPES.include?(doc.keys.last) && !doc.values.last.nil?)
+    def valid_media_type?(doc)
+      doc.is_a?(String) || (doc.is_a?(Hash) && VALID_MIME_TYPES.include?(doc.keys.last) && doc.values.last)
     end
 
-    def valid_link_property(link)
-      !link.attributes[:href].empty?
+    def valid_link_property?(link)
+      link && !link.attributes[:href].empty?
+    end
+
+    # check all rules surrounding transition based descriptors
+    def semantic_properties_check(descriptor, options, level)
+      if level != TOP_LEVEL
+        # all NON top level descriptors should have a sample entry
+        # TODO: look at rules for sample. For example, only single object based resources? Non list based transitions?
+        add_warning('descriptors.property_missing', options.merge({prop: 'sample'})) unless descriptor.sample
+      end
+    end
+
+    # check all rules surrounding transition based descriptors
+    def transition_properties_check(descriptor, options, level)
+      if level != TOP_LEVEL
+        # all NON top level descriptors should have a rt (return type) property
+        if descriptor.rt
+          # check if the return type is a valid local type to this file or an external return type
+          unless valid_return_type(descriptor.rt)
+             add_error('descriptors.invalid_return_type', options.merge({rt: descriptor.rt}))
+          end
+        else
+          add_error('descriptors.property_missing', options.merge({prop: 'rt'}))
+        end
+      end
+    end
+
+    def valid_return_type(return_type)
+      # if external, valid, assume http as only valid external for now
+      return true if return_type[/\Ahttp/] == 'http'
+      rt_is_a_valid_subresource(return_type)
+    end
+
+    def rt_is_a_valid_subresource(return_type)
+      resource_descriptor.states.include?(return_type)
+    end
+
+    #60, the descriptor hash of subresources must equal the state hash
+    def compare_with_state_resources
+      # TODO: change descriptor array into a hash with name as keys, or convert state names to an array of names
+      compare_with_other_hash(resource_descriptor.descriptor_document['descriptors'], resource_descriptor.states,
+                              'descriptors.descriptor_resource_not_found')
+      compare_with_other_hash(resource_descriptor.states, resource_descriptor.descriptor_document['descriptors'],
+                              'descriptors.state_resource_not_found')
+    end
+
+    def compare_with_other_hash(base_resources, others_resources, error)
+      base_resources.keys.each do |resource_name|
+        add_error(error, resource: resource_name, filename: filename) unless others_resources.include?(resource_name)
+      end
     end
 
     #61, descriptor transitions must match the transitions in the states and protocol sections
@@ -70,9 +150,10 @@ module Lint
       # then check if there is a transition missing for any state transition specified in the states: section
       build_protocol_transition_list.each do |transition|
         unless descriptor_transitions.include?(transition)
-          add_error('descriptors.protocol_transition_not_found', transition: transition,filename: filename)
+          add_error('descriptors.protocol_transition_not_found', transition: transition, filename: filename)
         end
       end
     end
+
   end
 end
--- spec/lib/lint/descriptor_spec.rb
+++ spec/lib/lint/descriptor_spec.rb
@@ -6,6 +6,118 @@ describe Lint do
     load_lint_translation_file
   end
 
+  it "displays a missing doc property error if a resource doc property is not specified" do
+    filename = lint_spec_filename('descriptor_section_errors', 'missing_doc_property.yml')
+
+    error =  expected_output(:error, 'descriptors.property_missing', resource: 'drds', prop: 'doc',
+      filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == error
+  end
+
+  it "displays a doc property error if a resource doc property is not a valid media type" do
+    filename = lint_spec_filename('descriptor_section_errors', 'invalid_doc_property.yml')
+
+    error =  expected_output(:error, 'descriptors.doc_media_type_invalid', resource: 'drds', media_type: 'html5',
+      filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == error
+  end
+
+  it "displays a doc property error if a resource doc property value is not specified" do
+    filename = lint_spec_filename('descriptor_section_errors', 'empty_doc_property.yml')
+
+    error =  expected_output(:error, 'descriptors.doc_media_type_invalid', resource: 'drds', media_type: 'html',
+      filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == error
+  end
+
+  it "displays a type property error if a resource type property is missing" do
+    filename = lint_spec_filename('descriptor_section_errors', 'missing_type_property.yml')
+
+    error =  expected_output(:error, 'descriptors.property_missing', resource: 'drds', prop: 'type',
+      filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == error
+  end
+
+  it "displays a type property error if a resource type property is not valid" do
+    filename = lint_spec_filename('descriptor_section_errors', 'invalid_type_property.yml')
+
+    error =  expected_output(:error, 'descriptors.type_invalid', resource: 'drds', type_prop: 'idemportant',
+      filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == error
+  end
+
+  it "displays a link property warning if a resource link property is missing" do
+    filename = lint_spec_filename('descriptor_section_errors', 'missing_link_property.yml')
+
+    warning =  expected_output(:warning, 'descriptors.property_missing', resource: 'drds', prop: 'link',
+      filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == warning
+  end
+
+  it "displays an invalid link self property error if a link self property is invalid" do
+    filename = lint_spec_filename('descriptor_section_errors', 'invalid_self_link_property.yml')
+
+    error =  expected_output(:error, 'descriptors.link_invalid', resource: 'drds', link: 'selff',
+      filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == error
+  end
+
+  it "displays an invalid link self property error if a link self property value is empty" do
+    filename = lint_spec_filename('descriptor_section_errors', 'empty_self_link_property.yml')
+
+    error =  expected_output(:error, 'descriptors.link_invalid', resource: 'drds', link: 'self',
+      filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == error
+  end
+
+  it "displays errors when the descriptor resource names do not match state resource names" do
+    filename = lint_spec_filename('descriptor_section_errors', 'mismatched_subresources.yml')
+
+    errors = expected_output(:error, 'descriptors.descriptor_resource_not_found', resource: 'dords',
+      filename: filename) <<
+      expected_output(:error, 'descriptors.state_resource_not_found', resource: 'drds', filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == errors
+  end
+
+  it "displays an invalid return type error when the descriptor return type is not valid" do
+    filename = lint_spec_filename('descriptor_section_errors', 'invalid_return_type.yml')
+
+    error =  expected_output(:error, 'descriptors.invalid_return_type', resource: 'create', rt: 'dord',
+      filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == error
+  end
+
+  it "displays a missing return type error when the descriptor return type is missing" do
+    filename = lint_spec_filename('descriptor_section_errors', 'missing_return_type.yml')
+
+    error =  expected_output(:error, 'descriptors.property_missing', resource: 'create', prop: 'rt',
+      filename: filename)
+
+    content = capture(:stdout) { Lint.validate(filename) }
+    content.should == error
+  end
+
+
   it "displays errors when the descriptor transitions list does not match state and protocol transitions" do
     filename = lint_spec_filename('descriptor_section_errors', 'missing_transitions.yml')
 
