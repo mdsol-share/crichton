56e89ac55a821745ee55557464c48117f2728bda
Chaim Solomon <csolomon@mdsol.com>
2013-09-23 13:49:44 +0300

    Added etag and last-modified conditional get and refactored a little.

--- lib/crichton/external_document_cache.rb
+++ lib/crichton/external_document_cache.rb
@@ -10,15 +10,58 @@ module Crichton
 
     def get(link)
       metadata = read_meta(link)
-      if metadata
-        read_datafile(link)
-      else
-        response = Net::HTTP.get_response(URI(link_without_fragment(link)))
-        write_response(link, response)
-      end
+      return read_datafile(link) if metadata && metadata_valid(metadata)
+      get_link_and_update_cache(link, metadata)
     end
 
     private
+    def metadata_valid(metadata)
+      #require 'pry'
+      #binding.pry
+      false
+    end
+
+    def metadata_etag(metadata)
+      metadata[:headers] && metadata[:headers]['etag']
+    end
+
+    def metadata_time(metadata)
+      metadata[:time]
+    end
+
+    def metadata_last_modified(metadata)
+      metadata[:headers] && metadata[:headers]['last-modified']
+    end
+
+    def get_link_and_update_cache(link, metadata=nil)
+      #response = Net::HTTP.get_response(URI(link_without_fragment(link)))
+      uri = URI(link_without_fragment(link))
+      request = Net::HTTP::Get.new(uri.request_uri)
+      if metadata
+        request['If-Modified-Since'] = metadata_last_modified(metadata).first if metadata_last_modified(metadata)
+        request['If-None-Match'] = metadata_etag(metadata).first if metadata_etag(metadata)
+      end
+      response = Net::HTTP.start(uri.hostname, uri.port) {|http| http.request(request) }
+
+      if response.code == '304'
+        # Unchanged
+        metadata[:time] = Time.now
+        File.open(metafile_path(link), 'wb') {|f| f.write(metadata.to_yaml) }
+        read_datafile(link)
+      else
+        # Fetched data
+        File.open(datafile_path(link), 'wb') {|f| f.write(response.body) }
+        new_metadata = {
+          link: link_without_fragment(link),
+          status: response.code,
+          headers: response.to_hash,
+          time: Time.now}
+        File.open(metafile_path(link), 'wb') {|f| f.write(new_metadata.to_yaml) }
+        response.body
+      end
+
+    end
+
     def filename_base_for_link(link)
       Digest::MD5.hexdigest(link_without_fragment(link))
     end
@@ -53,17 +96,6 @@ module Crichton
       else
         nil
       end
-    end
-
-    def write_response(link, response)
-      File.open(datafile_path(link), 'wb') {|f| f.write(response.body) }
-      metadata = {
-        link: link_without_fragment(link),
-        status: response.code,
-        headers: response.to_hash,
-        time: Time.now}
-      File.open(metafile_path(link), 'wb') {|f| f.write(metadata.to_yaml) }
-      response.body
     end
   end
 end
