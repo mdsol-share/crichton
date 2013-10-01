f53a37fcb5aa63731c88d1577ae926b518f10485
Chaim Solomon <csolomon@mdsol.com>
2013-10-01 16:30:29 +0300

    Added specs for additional rake-task related functionality

--- spec/lib/crichton/external_document_store_spec.rb
+++ spec/lib/crichton/external_document_store_spec.rb
@@ -52,5 +52,67 @@ module Crichton
         eds.get('http://some.server/some_link').should == nil
       end
     end
+
+    context '.download' do
+      before do
+        @pathname = 'pathname'
+        @response = double('Response')
+        @response.stub(:body).and_return('data')
+      end
+
+      it 'calls HTTP.start with the URL' do
+        Net::HTTP.should_receive(:start).with('hostname', 80).and_return(@response)
+        eds = ExternalDocumentStore.new(@pathname)
+        eds.download('http://hostname:80')
+      end
+
+      it 'returns the response body' do
+        Net::HTTP.stub(:start).and_return(@response)
+        eds = ExternalDocumentStore.new(@pathname)
+        eds.download('http://hostname:80').should == 'data'
+      end
+    end
+
+    context '.write_data_to_store' do
+      before do
+        @pathname = File.join('spec', 'fixtures', 'external_documents_store')
+        FileUtils.mkdir_p(@pathname) unless Dir.exists?(@pathname)
+        @datafilename = File.join(@pathname, 'hostname_80_path_to_somewhere.profile')
+        @metafilename = File.join(@pathname, 'hostname_80_path_to_somewhere.meta')
+        File.delete(@datafilename) if File.exist?(@datafilename)
+        File.delete(@metafilename) if File.exist?(@metafilename)
+      end
+
+      it 'writes the deta file' do
+        eds = ExternalDocumentStore.new(@pathname)
+        eds.write_data_to_store('http://hostname:80/path/to/somewhere', 'testdata')
+        File.open(@datafilename, 'rb') { |f| f.read}.should == 'testdata'
+      end
+
+      it 'writes the meta file' do
+        eds = ExternalDocumentStore.new(@pathname)
+        eds.write_data_to_store('http://hostname:80/path/to/somewhere', 'testdata')
+        File.open(@metafilename, 'rb') { |f| f.read}.should == 'http://hostname:80/path/to/somewhere'
+      end
+    end
+
+    context '.get_list_of_stored_links' do
+      before do
+        @pathname = File.join('spec', 'fixtures', 'external_documents_store')
+        FileUtils.mkdir_p(@pathname) unless Dir.exists?(@pathname)
+        FileUtils.rm Dir.glob(File.join(@pathname, '*.meta'))
+        @fixturelist = %w(test1 test2 test3)
+        @fixturelist.each { |fn| File.open(File.join(@pathname, "#{fn}.meta"), 'wb') { |f| f.write("Link #{fn}") }}
+      end
+
+      it 'returns a list of links (the content of the .meta files)' do
+        eds = ExternalDocumentStore.new(@pathname)
+        eds.get_list_of_stored_links.should == @fixturelist.map { |fn| "Link #{fn}" }
+      end
+
+      after do
+        FileUtils.rm Dir.glob(File.join(@pathname, '*.meta'))
+      end
+    end
   end
 end
