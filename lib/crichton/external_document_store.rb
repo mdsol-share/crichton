3d0b5e24631c09b06b0c417c081f9aee7f505392
Chaim Solomon <csolomon@mdsol.com>
2013-10-01 15:11:36 +0300

    Added rake task to download link and store it in the document store and rake task to check if the data is current.

--- lib/crichton/external_document_store.rb
+++ lib/crichton/external_document_store.rb
@@ -14,6 +14,22 @@ module Crichton
       read_datafile(link)
     end
 
+    def download(link)
+      uri = URI(link_without_fragment(link))
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(request) }
+      response.body
+    end
+
+    def write_data_to_store(link, data)
+      File.open(metafile_path(link), 'wb') { |f| f.write(link) }
+      File.open(datafile_path(link), 'wb') { |f| f.write(data) }
+    end
+
+    def get_list_of_stored_links
+      Dir.glob(File.join([@document_store_path, '*.meta'])).map { |n| File.open(n, 'rb') {|f| f.read}}
+    end
+
     private
     def datafile_path(link)
       url = URI.parse(link)
@@ -22,10 +38,23 @@ module Crichton
       File.join(@document_store_path, "#{snaked_host_port_and_path}.profile")
     end
 
+    def metafile_path(link)
+      url = URI.parse(link)
+      host_port_and_path = "#{url.host}:#{url.port.to_s}#{url.path}"
+      snaked_host_port_and_path = host_port_and_path.gsub(/\W/, '_')
+      File.join(@document_store_path, "#{snaked_host_port_and_path}.meta")
+    end
+
     def read_datafile(link)
       path = datafile_path(link)
       # This nil is important - it allows simple ||-ing of the store and cache.
       File.exists?(path) ? File.open(path, 'rb') { |f| f.read } : nil
+    end
+
+    def link_without_fragment(link)
+      parsed_link = Addressable::URI.parse(link)
+      parsed_link.fragment = nil
+      parsed_link.to_s
     end
   end
 end
--- tasks/alps.rake
+++ tasks/alps.rake
@@ -1,5 +1,6 @@
 require 'rake'
 require 'rake/clean'
+require 'crichton/external_document_store'
 
 begin
   namespace :alps do
@@ -11,6 +12,33 @@ begin
         %w(xml json).each do |ext|
           doc = Crichton.raw_profile_registry[key].send("to_#{ext}")
           File.open(File.join(directory, "#{key}.#{ext}"), 'w') { |f| f.write(doc) }
+        end
+      end
+    end
+
+    desc "Download ALPS profile documents to the external document store"
+    task :store_external_document, :link do |t, args|
+      store = Crichton::ExternalDocumentStore.new
+      new_data = store.download(args.link)
+      old_data = store.get(args.link)
+      write_data = true
+      if old_data && old_data != new_data
+        STDOUT.puts "The existing and downloaded data doesn't match. Are you sure you want to overwrite it? (y/n)"
+        input = STDIN.gets.strip
+        write_data = false unless input == 'y'
+      end
+      store.write_data_to_store(args.link, new_data) if write_data
+    end
+
+    desc "Download ALPS profile documents to the external document store"
+    task :check_external_documents do |t|
+      store = Crichton::ExternalDocumentStore.new
+      link_list = store.get_list_of_stored_links
+      link_list.each do |link|
+        new_data = store.download(link)
+        old_data = store.get(link)
+        if old_data != new_data
+          puts "Data of link #{link} has changed!"
         end
       end
     end
