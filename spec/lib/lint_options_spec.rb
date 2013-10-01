3cc2220059116fe957d580fae679436faad6ca98
Phil Servedio <pservedio@mdsol.com>
2013-10-01 14:35:15 -0700

    finish of lint and rake spec, bug fixes

--- spec/lib/lint_options_spec.rb
+++ spec/lib/lint_options_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'rake'
 require 'lint'
 
 describe Lint do
@@ -31,7 +32,7 @@ describe Lint do
     filename = lint_spec_filename('protocol_section_errors', 'no_entry_points.yml')
 
     expected_rdlint_output = expected_output(:error, 'protocols.entry_point_error', error: 'No', protocol: 'http',
-                                             filename: filename)
+      filename: filename)
 
     execution_output = %x(bundle exec rdlint #{filename})
     execution_output.should == expected_rdlint_output
@@ -50,7 +51,7 @@ describe Lint do
     filename = lint_spec_filename('protocol_section_errors', 'extraneous_properties.yml')
 
     expected_rdlint_output = capture(:stdout) { Lint.version } << expected_output(:warning,
-                                                                                  'protocols.extraneous_props', protocol: 'http', action: 'leviathan-link', filename: filename)
+      'protocols.extraneous_props', protocol: 'http', action: 'leviathan-link', filename: filename)
 
     execution_output = %x(bundle exec rdlint -v #{filename})
     execution_output.should == expected_rdlint_output
@@ -76,7 +77,7 @@ describe Lint do
     execution_output.should == "true\n"
   end
 
-  describe ".all_option" do
+  describe "rdlint_all_option" do
     # stub does not work in a new shell apparently, so a forced copy to the default api_descriptor dir is made
     before(:all) do
       %x(mkdir api_descriptors)
@@ -98,4 +99,88 @@ describe Lint do
     end
   end
 
+  describe ".rake_work" do
+    before(:all) do
+      Rake::Task.define_task(:environment)
+    end
+
+    before(:each) do
+      Rake::Task["crichton:lint"].reenable
+    end
+
+    it "allows users to use rake to validate a single descriptor file" do
+      filename = lint_spec_filename('protocol_section_errors', 'no_entry_points.yml')
+
+      expected_rake_output = "Linting file:'#{filename}'\n" <<
+        expected_output(:error, 'protocols.entry_point_error', error: 'No', protocol: 'http', filename: filename)
+
+      execution_output = capture(:stdout) { Rake.application.invoke_task "crichton:lint[#{filename}]" }
+      execution_output.should == expected_rake_output
+    end
+
+    it "displays empty output when all warnings are suppressed invoking rake with a warning free result" do
+      filename = lint_spec_filename('protocol_section_errors', 'extraneous_properties.yml')
+
+      expected_rake_output = "Linting file:'#{filename}'\nOptions: no_warnings\n" << "In file '#{filename}':\n"
+
+      execution_output = capture(:stdout) { Rake.application.invoke_task "crichton:lint[#{filename},no_warnings]" }
+      execution_output.should == expected_rake_output
+    end
+
+    it "displays empty output when all warnings are suppressed invoking rake with a warning free result" do
+      filename = lint_spec_filename('protocol_section_errors', 'extraneous_properties.yml')
+
+      expected_rake_output = "Linting file:'#{filename}'\nOptions: no_warnings\n" << "In file '#{filename}':\n"
+
+      execution_output = capture(:stdout) { Rake.application.invoke_task "crichton:lint[#{filename},no_warnings]" }
+      execution_output.should == expected_rake_output
+    end
+
+    it "returns a version number on an rake invocation with the version option" do
+      filename = lint_spec_filename('protocol_section_errors', 'extraneous_properties.yml')
+
+      expected_rake_output = capture(:stdout) { Lint.version } << "Linting file:'#{filename}'\nOptions: version\n" <<
+        expected_output(:warning, 'protocols.extraneous_props', protocol: 'http', action: 'leviathan-link',
+        filename: filename)
+
+      execution_output = capture(:stdout) { Rake.application.invoke_task "crichton:lint[#{filename},version]" }
+      execution_output.should == expected_rake_output
+    end
+
+    it 'returns false on an rake invocation with the strict option' do
+      filename = lint_spec_filename('protocol_section_errors', 'missing_protocol_actions.yml')
+      expected_rake_output = "Linting file:'#{filename}'\nOptions: strict\nfalse\n"
+      execution_output = capture(:stdout) { Rake.application.invoke_task "crichton:lint[#{filename},strict]" }
+      execution_output.should == expected_rake_output
+    end
+
+    it 'returns true in strict mode when a clean descriptor file is validated via rake' do
+      filename = lint_spec_filename('protocol_section_errors', 'extraneous_properties.yml')
+      expected_rake_output = "Linting file:'#{filename}'\nOptions: strict\ntrue\n"
+      execution_output = capture(:stdout) { Rake.application.invoke_task "crichton:lint[#{filename},strict]" }
+      execution_output.should == expected_rake_output
+    end
+
+    describe "rake_all_option" do
+      before(:all) do
+        %x(mkdir api_descriptors)
+        %x(cp spec/fixtures/lint_resource_descriptors/missing_sections/* api_descriptors)
+      end
+
+      after(:all) do
+        %x(rm -rf api_descriptors)
+      end
+
+      it 'invokes the all aption and processes all the files in the config folder' do
+        execution_output = capture(:stdout) { Rake.application.invoke_task "crichton:lint[all]" }
+        file_segment = "In file '"<< default_lint_descriptor_file('nostate_descriptor.yml') << "'"
+        execution_output.should include(file_segment)
+        file_segment = "In file '"<< default_lint_descriptor_file('noprotocols_descriptor.yml') << "'"
+        execution_output.should include(file_segment)
+        file_segment = "In file '"<< default_lint_descriptor_file('nodescriptors_descriptor.yml') << "'"
+        execution_output.should include(file_segment)
+      end
+    end
+
+  end
 end
--- tasks/lint.rake
+++ tasks/lint.rake
@@ -8,15 +8,18 @@ namespace :crichton do
       if args[:lint_options] == 'version'
         Lint.version
       end
+      retval = nil
       if args[:file_or_all] == 'all'
         puts "Linting all descriptor files"
         puts "Options: #{args[:lint_options]}" if args[:lint_options]
-        Lint.validate_all(args[:lint_options] ? args[:lint_options] : {})
+        retval = Lint.validate_all(args[:lint_options] ? args[:lint_options] : {})
       else
         puts "Linting file:'#{args[:file_or_all]}'"
         puts "Options: #{args[:lint_options]}" if args[:lint_options]
-        Lint.validate(args[:file_or_all], args[:lint_options] ? args[:lint_options] : {})
+        retval = Lint.validate(args[:file_or_all], args[:lint_options] ? args[:lint_options] : {})
       end
+      puts retval.to_s if args[:lint_options] == 'strict'
+      retval
     rescue StandardError => e
       puts "Lint exception: #{e.message}"
     end
