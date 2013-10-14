2cfabe12afcfa7f45f7ec78b3259f3d6e3142d51
abrutski <abrutski@mdsol.com>
2013-10-14 14:53:18 -0400

    adding support for POST, PUT, DELETE specs

--- spec/lib/core_ext/action_controller_spec.rb
+++ spec/lib/core_ext/action_controller_spec.rb
@@ -15,8 +15,11 @@ describe 'ActionController' do
     @controller = Support::Controllers::TestController.new
     @controller.request = ActionController::TestRequest.new
     @controller.response = ActionController::TestResponse.new
-    @controller.request.accept = 'text/html'
+    @controller.request.accept = 'application/sample_type'
     @model = double('model')
+    @model.class_eval do
+      include Crichton::Representor
+    end
   end
 
   after (:all) do
@@ -30,16 +33,13 @@ describe 'ActionController' do
 
     context 'when it is not a crichton representor model' do
       it 'attempts to render html template and fails' do
-        expect { @controller.show(@model) }.to raise_error { ActionView::MissingTemplate }
+        @controller.request.accept = 'text/html'
+        expect { @controller.show(double('model')) }.to raise_error { ActionView::MissingTemplate }
       end
     end
 
     context 'when it is a crichton representor model' do
       it 'calls to_media_type' do
-        @model.class_eval do
-          include Crichton::Representor
-        end
-        @controller.request.accept = 'application/sample_type'
         @model.should_receive(:to_media_type).and_return(anything())
         @controller.show(@model)
       end
@@ -54,19 +54,13 @@ describe 'ActionController' do
 
     context 'when it is not a crichton representor model' do
       it 'redirects_to #show' do
+        @controller.request.accept = 'text/html'
         @controller.should_receive(:redirect_to).with('show').and_return(anything())
-        @controller.create(@model)
+        @controller.create(double('model'))
       end
     end
 
     context 'when it is a crichton representor model' do
-      before do
-        @controller.request.accept = 'application/sample_type'
-        @model.class_eval do
-          include Crichton::Representor
-        end
-      end
-
       it 'calls render method with 201 status code' do
         @controller.should_receive(:render).with(hash_including(status: :created)).and_return(anything())
         @controller.create(@model)
@@ -76,6 +70,40 @@ describe 'ActionController' do
         @model.should_receive(:to_media_type).and_return(anything())
         @controller.create(@model)
       end
+    end
+  end
+
+  describe '#update' do
+    before do
+      @controller.action_name = 'update'
+      @controller.request.request_method = 'PUT'
+    end
+
+    context 'when it is not a crichton representor model' do
+      it 'redirects_to #show' do
+        @controller.request.accept = 'text/html'
+        @controller.should_receive(:redirect_to).with('show').and_return(anything())
+        @controller.update(double('model'))
+      end
+    end
+
+    context 'when it is a crichton representor model' do
+      it 'calls render method with 200 status code' do
+        @controller.should_receive(:render).with(hash_including(status: :ok)).and_return(anything())
+        @controller.update(@model)
+      end
+    end
+  end
+
+  describe '#destroy' do
+    before do
+      @controller.action_name = 'destroy'
+      @controller.request.request_method = 'DELETE'
+    end
+
+    it 'returns 204 status code' do
+      @controller.destroy(@model)
+      @controller.response.status.should equal(204)
     end
   end
 end
--- spec/support/controllers.rb
+++ spec/support/controllers.rb
@@ -11,6 +11,14 @@ module Support
       def create(model)
         respond_with(model, status: :created, location: 'show')
       end
+
+      def update(model)
+        respond_with(model, status: :ok, location: 'show')
+      end
+
+      def destroy(model)
+        head :no_content
+      end
     end
   end
 end
