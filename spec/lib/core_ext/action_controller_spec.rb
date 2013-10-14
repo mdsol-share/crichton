716b7c1acd4edf2ccfa62c930db69193099be6e7
abrutski <abrutski@mdsol.com>
2013-10-14 12:35:54 -0400

    adding specs for #create action

--- spec/lib/core_ext/action_controller_spec.rb
+++ spec/lib/core_ext/action_controller_spec.rb
@@ -45,4 +45,37 @@ describe 'ActionController' do
       end
     end
   end
+
+  describe '#create' do
+    before do
+      @controller.action_name = 'create'
+      @controller.request.request_method = 'POST'
+    end
+
+    context 'when it is not a crichton representor model' do
+      it 'redirects_to #show' do
+        @controller.should_receive(:redirect_to).with('show').and_return(anything())
+        @controller.create(@model)
+      end
+    end
+
+    context 'when it is a crichton representor model' do
+      before do
+        @controller.request.accept = 'application/sample_type'
+        @model.class_eval do
+          include Crichton::Representor
+        end
+      end
+
+      it 'calls render method with 201 status code' do
+        @controller.should_receive(:render).with(hash_including(status: :created)).and_return(anything())
+        @controller.create(@model)
+      end
+
+      it 'calls to_media_type' do
+        @model.should_receive(:to_media_type).and_return(anything())
+        @controller.create(@model)
+      end
+    end
+  end
 end
--- spec/support/controllers.rb
+++ spec/support/controllers.rb
@@ -3,8 +3,13 @@ module Support
   module Controllers
     class TestController < ActionController::Base
       respond_to :html, :sample_type
+
       def show(model)
         respond_with(model)
+      end
+
+      def create(model)
+        respond_with(model, status: :created, location: 'show')
       end
     end
   end
