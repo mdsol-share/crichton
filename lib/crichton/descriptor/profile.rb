7959da7dd1203a94d472b06203d2ad1137879f25
Chaim Solomon <csolomon@mdsol.com>
2013-10-14 18:39:43 +0300

    Addressed a few comments

--- lib/crichton/descriptor/profile.rb
+++ lib/crichton/descriptor/profile.rb
@@ -24,14 +24,14 @@ module Crichton
       # @return [Array] The descriptor instances.
 
       def self_transition
-        descriptors unless @self_descriptor
-        @self_descriptor
+        descriptors unless @self_transition
+        @self_transition
       end
 
       def descriptors
         @descriptors[:all] ||= (descriptor_document['descriptors'] || {}).keys.map do |id|
           descriptor = Detail.new(resource_descriptor, self, id)
-          @self_descriptor = descriptor if descriptor.name == 'self' && TRANSITION_TYPES.include?(descriptor.type)
+          @self_transition = descriptor if descriptor.name == 'self' && descriptor.transition?
           descriptor
         end.freeze
       end
