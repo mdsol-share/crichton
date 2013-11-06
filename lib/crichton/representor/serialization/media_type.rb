a1227c772ac7c5a73a952bbb486ce5ecf3e14ae9
Chaim Solomon <csolomon@mdsol.com>
2013-11-06 13:18:20 +0200

    Added documentation on the options hash content

--- lib/crichton/representor/serialization/media_type.rb
+++ lib/crichton/representor/serialization/media_type.rb
@@ -25,13 +25,26 @@ module Crichton
         # convert the representation into the base media-type. For example, for a HAL JSON serializer, #as_media_type
         # would return a hash, but #to_media_type would return a JSON string.
         #
+        # media_type (for now) can be :xhtml or :html
+        #
         # The options hash may contain:
+        #
+        # :conditions => :condition
+        #
+        #   The conditions are defined in the states section of the descriptor document. See the
+        #   {file:doc/state_descriptors.md state descriptors documentation} for more information on that topic.
+        #
+        # :semantics => :styled_microdata
+        #
+        #   semantics indicates the semantic markup type to apply. Valid options are
+        #   :microdata and :styled_microdata. If not included, defaults to :microdata.
+        #
         # :embed_optional => {'name1' => :embed, 'name2' => :link}
         #
-        # The keys need to be strings which correspond to the name of the attribute that has an embed: single-optional
-        # or multiple-optional or single-optional-link or multiple-optional-link. The first two embed values (the ones
-        # without -link) default to embed when no embed_optional parameter is specified, the ones with -link default
-        # to embedding a link.
+        #   The keys need to be strings which correspond to the name of the attribute that has an embed: single-optional
+        #   or multiple-optional or single-optional-link or multiple-optional-link. The first two embed values (the ones
+        #   without -link) default to embed when no embed_optional parameter is specified, the ones with -link default
+        #   to embedding a link.
         #
         # @param [Symbol, String] media_type The registered media-type associated with the desired serializer.
         # @param [Hash] options Conditional options to configure to the serialization.
