1fa3a94222e3b538bbaa0e0a97cd5cfeb4d54c51
Phil Servedio <pservedio@mdsol.com>
2014-01-06 15:27:44 -0800

    Alias to_media_type for cleaner implementation of entry_pts

--- lib/crichton/discovery/entry_points.rb
+++ lib/crichton/discovery/entry_points.rb
@@ -26,7 +26,7 @@ module Crichton
       #
       # @param media_type [Symbol] :json_home or :html
       # @param resource_relation [Hash] Hash of options for serialization
-      def as_media_type(media_type, options)
+      def as_media_type(media_type, options = {})
         options = options.merge({semantics: :microdata}) if media_type == :xhtml
         case media_type
         when :html, :xhtml
@@ -37,15 +37,7 @@ module Crichton
         end
       end
 
-      ##
-      # Returns a string representing a serialization.
-      #
-      # @param [Hash] options Optional configurations.
-      #
-      # @return [Hash] The built representation.
-      def to_media_type(media_type, options = {})
-        as_media_type(media_type, options)
-      end
+      alias :to_media_type :as_media_type
     end
   end
 end
