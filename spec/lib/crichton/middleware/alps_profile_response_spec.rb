f26b167327f822b9201a9289e7427d7d5e95baa5
Shea Valentine <svalentine@mdsol.com>
2014-02-19 10:48:46 -0500

    formatting

--- spec/lib/crichton/middleware/alps_profile_response_spec.rb
+++ spec/lib/crichton/middleware/alps_profile_response_spec.rb
@@ -31,107 +31,108 @@ module Crichton
         let(:env) do
             Rack::MockRequest.env_for(@uri).tap { |e| e["HTTP_ACCEPT"] = @media_type }
         end
+        
         context 'when the alps path' do
-            %w(text/html application/alps+xml).each do |media_type|
-              it "responds with an alps document associated with the profile id for #{media_type} requests" do
-                @media_type = media_type
-                @expires =  (Time.new + ten_minutes).httpdate
-                @uri = "#{base_uri}/DRDs"
-                home_responder.call(env).should == [200, headers, [alps_xml_data]]
-              end
-            end
-
-            it 'responds with an alps document associated with the profile id for application/alps+json requests' do
-              @media_type = 'application/alps+json'
+          %w(text/html application/alps+xml).each do |media_type|
+            it "responds with an alps document associated with the profile id for #{media_type} requests" do
+              @media_type = media_type
               @expires =  (Time.new + ten_minutes).httpdate
               @uri = "#{base_uri}/DRDs"
-              # go get the body of the response and JSON parse it.
-              obj = JSON.parse(home_responder.call(env)[2].first)
-              body = JSON.pretty_generate(obj)
-              body.should be_json_eql(alps_json_data)
-            end
-
-            it 'uses the first supported media type in the HTTP_ACCEPT header' do
-              @media_type = 'bogus/media_type,*/a,text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*'
-              @uri = "#{base_uri}/DRDs"
-              first_content_type_header = {'Content-Type' => 'application/xml',
-                'expires' => (Time.new + ten_minutes).httpdate}
-              home_responder.call(env).should == [200, first_content_type_header, [alps_xml_data]]
-            end
-
-            it 'responds correctly with a non standard HTTP_ACCEPT header' do
-              @media_type = 'bogus/media_type, text/html,  application/xhtml+xml, application/xml;q=0.9,  image/webp, */*'
-              @uri = "#{base_uri}/DRDs"
-              first_content_type_header = {'Content-Type' => 'application/xml',
-                'expires' => (Time.new + ten_minutes).httpdate}
-              home_responder.call(env).should == [200, first_content_type_header, [alps_xml_data]]
-            end
-
-            %w(text/html application/alps+xml application/alps+json).each do |media_type|
-              it "responds with the expected content-type for #{media_type} requests" do
-                @media_type = media_type
-                @uri = "#{base_uri}/DRDs"
-                response = home_responder.call(env)
-                media_type = 'application/xml' if media_type == 'text/html'
-                response[1]['Content-Type'].should == media_type
-              end
-            end
-
-            it 'responds with the correct expiration date when it a timeout is specified as an option' do
-              responder = AlpsProfileResponse.new(rack_app, {'expiry' => 20}) #minutes instead of default of 10
-              @media_type = 'text/html'
-              @expires = (Time.new + 1200).httpdate
-              @uri = "#{base_uri}/DRDs"
-              responder.call(env).should == [200, headers, [alps_xml_data]]
-            end
-
-            it 'responds with the correct expiration date when a symbolized timeout in specified' do
-              responder = AlpsProfileResponse.new(rack_app, {:expiry => 20}) #minutes instead of default of 10
-              @media_type = 'text/html'
-              @expires = (Time.new + 1200).httpdate
-              @uri = "#{base_uri}/DRDs"
-              responder.call(env).should == [200, headers, [alps_xml_data]]
-            end
-
-            it 'returns a 406 status for unsupported media_types' do
-              @media_type = 'application/jrd+json'
-              @uri = "#{base_uri}/DRDs"
-              home_responder.call(env)[0].should == 406
-            end
-
-            it 'returns a 406 status for an empty list of acceptable media types' do
-              @media_type = ''
-              @uri = "#{base_uri}/DRDs"
-              home_responder.call(env)[0].should == 406
-            end
-
-            it 'returns a 404 if the profile in the request is not valid' do
-              @media_type = 'text/html'
-              @uri = "#{base_uri}/BLAH"
-              home_responder.call(env) == [404, {'Content-Type' => 'text/html'}, ["Profile BLAH not found"]]
-            end
-
-            it 'returns a 404 if the alps request contains no resource' do
-              @media_type = 'text/html'
-              @uri = "#{base_uri}"
-              home_responder.call(env) == [404, {'Content-Type' => 'text/html'}, ["Profile not found"]]
-            end
-
-            it 'successfully responds to various alps paths' do
-              @media_type = 'text/html'
-              @expires =  (Time.new + ten_minutes).httpdate
-              %w(DRDs DRDs/ DRDs#list).each do |path_segment|
-                @uri =  "#{config.alps_base_uri}/#{path_segment}"
-                home_responder.call(env).should == [200, headers, [alps_xml_data]]
-              end
-            end
-            
-            it 'invokes the parent rack app from the middleware' do
-              @media_type = 'text/html'
-              @uri = "some.other.url/DRDs"
-              home_responder.call(env)[0].should == 202
+              home_responder.call(env).should == [200, headers, [alps_xml_data]]
             end
           end
+
+          it 'responds with an alps document associated with the profile id for application/alps+json requests' do
+            @media_type = 'application/alps+json'
+            @expires =  (Time.new + ten_minutes).httpdate
+            @uri = "#{base_uri}/DRDs"
+            # go get the body of the response and JSON parse it.
+            obj = JSON.parse(home_responder.call(env)[2].first)
+            body = JSON.pretty_generate(obj)
+            body.should be_json_eql(alps_json_data)
+          end
+
+          it 'uses the first supported media type in the HTTP_ACCEPT header' do
+            @media_type = 'bogus/media_type,*/a,text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*'
+            @uri = "#{base_uri}/DRDs"
+            first_content_type_header = {'Content-Type' => 'application/xml',
+              'expires' => (Time.new + ten_minutes).httpdate}
+            home_responder.call(env).should == [200, first_content_type_header, [alps_xml_data]]
+          end
+
+          it 'responds correctly with a non standard HTTP_ACCEPT header' do
+            @media_type = 'bogus/media_type, text/html,  application/xhtml+xml, application/xml;q=0.9,  image/webp, */*'
+            @uri = "#{base_uri}/DRDs"
+            first_content_type_header = {'Content-Type' => 'application/xml',
+              'expires' => (Time.new + ten_minutes).httpdate}
+            home_responder.call(env).should == [200, first_content_type_header, [alps_xml_data]]
+          end
+
+          %w(text/html application/alps+xml application/alps+json).each do |media_type|
+            it "responds with the expected content-type for #{media_type} requests" do
+              @media_type = media_type
+              @uri = "#{base_uri}/DRDs"
+              response = home_responder.call(env)
+              media_type = 'application/xml' if media_type == 'text/html'
+              response[1]['Content-Type'].should == media_type
+            end
+          end
+
+          it 'responds with the correct expiration date when it a timeout is specified as an option' do
+            responder = AlpsProfileResponse.new(rack_app, {'expiry' => 20}) #minutes instead of default of 10
+            @media_type = 'text/html'
+            @expires = (Time.new + 1200).httpdate
+            @uri = "#{base_uri}/DRDs"
+            responder.call(env).should == [200, headers, [alps_xml_data]]
+          end
+
+          it 'responds with the correct expiration date when a symbolized timeout in specified' do
+            responder = AlpsProfileResponse.new(rack_app, {:expiry => 20}) #minutes instead of default of 10
+            @media_type = 'text/html'
+            @expires = (Time.new + 1200).httpdate
+            @uri = "#{base_uri}/DRDs"
+            responder.call(env).should == [200, headers, [alps_xml_data]]
+          end
+
+          it 'returns a 406 status for unsupported media_types' do
+            @media_type = 'application/jrd+json'
+            @uri = "#{base_uri}/DRDs"
+            home_responder.call(env)[0].should == 406
+          end
+
+          it 'returns a 406 status for an empty list of acceptable media types' do
+            @media_type = ''
+            @uri = "#{base_uri}/DRDs"
+            home_responder.call(env)[0].should == 406
+          end
+
+          it 'returns a 404 if the profile in the request is not valid' do
+            @media_type = 'text/html'
+            @uri = "#{base_uri}/BLAH"
+            home_responder.call(env) == [404, {'Content-Type' => 'text/html'}, ["Profile BLAH not found"]]
+          end
+
+          it 'returns a 404 if the alps request contains no resource' do
+            @media_type = 'text/html'
+            @uri = "#{base_uri}"
+            home_responder.call(env) == [404, {'Content-Type' => 'text/html'}, ["Profile not found"]]
+          end
+
+          it 'successfully responds to various alps paths' do
+            @media_type = 'text/html'
+            @expires =  (Time.new + ten_minutes).httpdate
+            %w(DRDs DRDs/ DRDs#list).each do |path_segment|
+              @uri =  "#{config.alps_base_uri}/#{path_segment}"
+              home_responder.call(env).should == [200, headers, [alps_xml_data]]
+            end
+          end
+          
+          it 'invokes the parent rack app from the middleware' do
+            @media_type = 'text/html'
+            @uri = "some.other.url/DRDs"
+            home_responder.call(env)[0].should == 202
+          end
+        end
       end
       
       describe '#call' do
