75d3ab030b20f232158033b065d33c8fb7ac16be
Jordi Carres <mumismo@gmail.com>
2014-04-15 17:49:26 +0900

    added a generator and a template for Rails

---
+++ lib/crichton/rails/generators/resource_description_generator.rb
@@ -0,0 +1,22 @@
+module Crichton
+  # registers a Generator with Rails invoked as crichton:resource_description
+  class ResourceDescriptionGenerator <  Rails::Generators::Base
+    # The source directory for our templates
+    source_root File.expand_path("../", __FILE__)
+
+    argument :resource_name, type: :string, desc: "The name for the resource"
+    argument :collection_name, type: :string, desc: "The name for the collection resource"
+
+    desc "This generator will generate a resource description file."
+    def create_description
+      copy_file "template.yaml", filename
+      gsub_file filename, "{resource_name}", resource_name
+      gsub_file filename, "{collection_name}", collection_name
+    end
+
+    private
+    def filename
+       "api_descriptors/#{collection_name}.yaml"
+     end
+  end
+end
