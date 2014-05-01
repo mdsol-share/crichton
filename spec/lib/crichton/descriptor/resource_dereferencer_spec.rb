20b5ab78ccada8229675f7f5763fbe77d0cb67f9
brutski <abrutski@mdsol.com>
2014-05-01 11:53:15 -0400

    added resource_dereference class and specs

---
+++ spec/lib/crichton/descriptor/resource_dereferencer_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+require 'crichton/descriptor/resource_dereferencer'
+
+module Crichton
+  module Descriptor
+    describe ResourceDereferencer do
+      before(:all) do
+        @resource_descriptor = '
+id: DRDs
+semantics:
+  name:
+    href: http://alps.io/schema.org/Text
+idempotent:
+  update:
+    parameters:
+      - href: name'
+        @hash = YAML.load(@resource_descriptor)
+      end
+
+      let(:subject) { ResourceDereferencer.new(@hash) }
+
+      describe '#initialize' do
+        it 'returns the descriptor document passed to constructor' do
+          expect(subject.resource_document).to eq(@hash)
+        end
+
+        it 'returns document id found in descriptor document' do
+          expect(subject.resource_id).to eq('DRDs')
+        end
+      end
+
+      describe '#resource_descriptors' do
+        let(:resource_descriptors) { subject.resource_descriptors }
+
+        it 'registers all available descriptor elements in the document' do
+          expect(resource_descriptors.keys).to eq([ 'DRDs#name', 'DRDs#update' ])
+        end
+
+        it 'registers descriptors elements in registry' do
+          resource_descriptors.values.each { |v| expect(v).to be_a Crichton::Descriptor::DescriptorElement }
+        end
+      end
+    end
+  end
+end
