fefe866dbb4fe14ccfea763ebf0b633919f45dc3
brutski <abrutski@mdsol.com>
2014-05-15 18:49:25 -0400

    states_validator_spec fixes

--- spec/lib/crichton/lint/states_validator_spec.rb
+++ spec/lib/crichton/lint/states_validator_spec.rb
@@ -5,11 +5,21 @@ module Crichton
   module Lint
     describe StatesValidator do
       let(:validator) { Crichton::Lint }
-      let(:filename) { lint_spec_filename(*@filename) }
+      let(:filename) { create_drds_file(@descriptor, @filename) }
+
+      before(:all) do
+        @filename = 'drds_lint.yml'
+      end
 
       describe '#validate' do
+        before do
+          Crichton::ExternalDocumentStore.any_instance.stub(:get).with(an_instance_of(Addressable::URI)).and_return('<alps></alps>')
+          Crichton::ExternalDocumentStore.any_instance.stub(:get).with(/alps/).and_return('<alps></alps>')
+          Crichton::ExternalDocumentStore.any_instance.stub(:get).with('http://schema.org/Something').and_return(nil)
+        end
+
         after do
-          validation_report.should == (@errors || @warnings || @message)
+          expect(validation_report).to eq(@errors || @warnings || @message)
         end
 
         def validation_report
@@ -17,14 +27,21 @@ module Crichton
         end
 
         it 'reports warnings correlating to self: and doc: issues' do
-          @filename = %w(state_section_errors condition_doc_and_self_errors.yml)
+          @descriptor = new_drds_descriptor.tap do |document|
+            document['resources']['drds']['states']['collection']['transitions']['list'].merge!({ 'name' => 'selff' })
+            document['resources']['drds']['states']['collection']['transitions']['create'].merge!({ 'name' => 'self' })
+            document['resources']['drd']['states']['activated'].except!('doc')
+          end
           @warnings = expected_output(:warning, 'states.no_self_property', resource: 'drds', state: 'collection',
             transition: 'list', filename: filename, section: :states, sub_header: :warning) <<
             expected_output(:warning, 'states.doc_property_missing', resource: 'drd', state: 'activated')
         end
 
         it 'reports errors when next transitions are missing or empty' do
-          @filename = %w(state_section_errors missing_and_empty_transitions.yml)
+          @descriptor = new_drds_descriptor.tap do |document|
+            document['resources']['drds']['states']['collection']['transitions']['list'].except!('next')
+            document['resources']['drd']['states']['activated']['transitions']['show'].except!('next')
+          end
           @errors = expected_output(:error, 'states.empty_missing_next', resource: 'drds', state: 'collection',
             transition: 'list', filename: filename, section: :states, sub_header: :error) <<
             expected_output(:error, 'states.empty_missing_next', resource: 'drd', state: 'activated',
@@ -32,7 +49,10 @@ module Crichton
         end
 
         it 'reports errors when next transitions are pointing to non-existent states' do
-          @filename = %w(state_section_errors phantom_transitions.yml)
+          @descriptor = new_drds_descriptor.tap do |document|
+            document['resources']['drds']['states']['navigation']['transitions']['search']['next'] = ['navegation']
+            document['resources']['drd']['states']['activated']['transitions']['show']['next'] = ['activate']
+          end
           @errors = expected_output(:error, 'states.phantom_next_property', secondary_descriptor: 'drds',
             state: 'navigation', transition: 'self', next_state: 'navegation', filename: filename, section: :states,
             sub_header: :error) <<
@@ -41,69 +61,88 @@ module Crichton
         end
 
         it 'reports errors when states transitions does not match protocol or descriptor transitions' do
-          @filename = %w(state_section_errors missing_transitions.yml)
+          @descriptor = new_drds_descriptor.tap do |document|
+            document['resources']['drds']['states']['collection']['transitions'].except!('create')
+            document['resources']['drds']['states']['navigation']['transitions'].except!('create')
+          end
           @errors = expected_output(:error, 'states.descriptor_transition_not_found', transition: 'create',
             filename: filename, section: :states, sub_header: :error) <<
             expected_output(:error, 'states.protocol_transition_not_found', transition: 'create')
         end
 
         it 'reports a warning when a transition-less state does not contain a location' do
-          @filename = %w(state_section_errors missing_location.yml)
-          @warnings = expected_output(:warning, 'states.location_property_missing', resource: 'drds', state: 'deleted',
+          @descriptor = new_drds_descriptor.tap do |document|
+            document['resources']['drd']['states']['deleted'].except!('location')
+          end
+          @warnings = expected_output(:warning, 'states.location_property_missing', resource: 'drd', state: 'deleted',
             filename: filename, section: :states, sub_header: :warning)
         end
 
-        it 'reports an error if a resource has no states defined' do
-          @filename = %w(state_section_errors no_states_defined.yml)
-          @errors = expected_output(:error, 'catastrophic.no_states', resource: 'drd',  filename: filename,
-            section: :states, sub_header: :error)
-        end
-
         it 'reports an error if no state transitions define "name:self" property' do
-          @filename = %w(state_section_errors no_names_defined.yml)
+          @descriptor = new_drds_descriptor.tap do |document|
+            document['resources']['drds']['states']['collection']['transitions']['list'].except!('name')
+          end
           @errors = expected_output(:error, 'states.name_self_exception', filename: filename,
             section: :states, sub_header: :error, state: 'collection')
         end
 
         it 'reports an error if two or more state transitions define "name:self"' do
-          @filename = %w(state_section_errors two_transitions_per_state_define_self.yml)
+          @descriptor = new_drds_descriptor.tap do |document|
+            document['resources']['drds']['states']['collection']['transitions']['create'].merge!({ 'name' => 'self' })
+          end
           @errors = expected_output(:error, 'states.name_self_exception', filename: filename,
             section: :states, sub_header: :error, state: 'collection')
         end
 
         it 'reports an error if there are duplicated "name" properties per state' do
-          @filename = %w(state_section_errors duplicated_transition_names.yml)
+          @descriptor = new_drds_descriptor.tap do |document|
+            document['resources']['drds']['states']['collection']['transitions']['search'].merge!({ 'name' => 'transition' })
+            document['resources']['drds']['states']['collection']['transitions']['create'].merge!({ 'name' => 'transition' })
+          end
           @errors = expected_output(:error, 'states.name_duplicated_exception', filename: filename,
             section: :states, sub_header: :error, state: 'collection', transition: 'transition') <<
               expected_output(:warning, 'states.no_self_property', resource: 'drds', state: 'collection',
-            transition: 'find', sub_header: :warning) <<
+            transition: 'search', sub_header: :warning) <<
               expected_output(:warning, 'states.no_self_property', resource: 'drds', state: 'collection',
             transition: 'create')
         end
 
         context 'an external profile' do
-          let(:external_url) { 'http://alps.io/schema.org/Leviathans' }
+          let(:external_url) { 'http://schema.org/Something' }
 
-           it 'reports no errors when it is already downloaded to disk' do
-            Crichton::ExternalDocumentStore.any_instance.stub(:get).and_return('<alps></alps>')
-            @filename = %w(clean_descriptor_file.yml)
+          it 'reports no errors when it is already downloaded to disk' do
+            @descriptor = new_drds_descriptor
             @message = "In file '#{filename}':\n#{I18n.t('aok').green}\n"
           end
 
           it 'reports an error if its url points to an invalid address' do
             stub_profile_request(404)
-            @filename = %w(state_section_errors external_profile.yml)
+            @descriptor = new_drds_descriptor.tap do |document|
+              external_location = [{ 'location' => external_url }]
+              document['resources']['drd']['states']['activated']['transitions']['repair-history']['next'] = external_location
+            end
             @errors = expected_output(:error, 'states.invalid_external_location', link: external_url,
-              secondary_descriptor: 'drd', state: 'activated', transition: 'self', filename: filename, section: :states,
-              sub_header: :error)
+              secondary_descriptor: 'drd', state: 'activated', transition: 'leviathan', filename: filename, section: :states,
+              sub_header: :error) <<
+              expected_output(:error, 'states.invalid_external_location', link: external_url,
+                secondary_descriptor: 'drd', state: 'activated', transition: 'repair-history') <<
+              expected_output(:error, 'states.invalid_external_location', link: external_url,
+                secondary_descriptor: 'drd', state: 'deactivated', transition: 'leviathan')
           end
 
           it 'reports a warning if its url has a valid address but is not downloaded to disk' do
             stub_profile_request(200)
-            @filename = %w(state_section_errors external_profile.yml)
+            @descriptor = new_drds_descriptor.tap do |document|
+              external_location = [{ 'location' => external_url }]
+              document['resources']['drd']['states']['activated']['transitions']['repair-history']['next'] = external_location
+            end
             @warnings = expected_output(:warning, 'states.download_external_profile', link: external_url,
-              secondary_descriptor: 'drd', state: 'activated', transition: 'self', filename: filename, section: :states,
-              sub_header: :warning)
+              secondary_descriptor: 'drd', state: 'activated', transition: 'leviathan', filename: filename, section: :states,
+              sub_header: :warning) <<
+              expected_output(:warning, 'states.download_external_profile', link: external_url,
+                secondary_descriptor: 'drd', state: 'activated', transition: 'repair-history') <<
+              expected_output(:warning, 'states.download_external_profile', link: external_url,
+                secondary_descriptor: 'drd', state: 'deactivated', transition: 'leviathan')
           end
 
           def stub_profile_request(status)
