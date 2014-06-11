7fa527151c5c253d75074c24f427654fd9129401
brutski <abrutski@mdsol.com>
2014-06-10 22:47:39 -0400

    include_alps_middleware and include_discovery_middleware are predicates now

--- spec/lib/crichton/configuration_spec.rb
+++ spec/lib/crichton/configuration_spec.rb
@@ -14,19 +14,19 @@ module Crichton
     describe '#include_discovery_middleware' do
       context 'when configured' do
         it 'returns true when configured value is true' do
-          expect(configuration.include_discovery_middleware).to be_true
+          expect(configuration.include_discovery_middleware?).to be_true
         end
 
         it 'returns false when configured value is false' do
           @config = example_environment_config.merge({ 'include_discovery_middleware' => false })
-          expect(configuration.include_discovery_middleware).to be_false
+          expect(configuration.include_discovery_middleware?).to be_false
         end
       end
 
       context 'when not configured' do
         it 'returns false' do
           @config = example_environment_config.except('include_discovery_middleware')
-          expect(configuration.include_discovery_middleware).to be_false
+          expect(configuration.include_discovery_middleware?).to be_false
         end
       end
     end
@@ -34,19 +34,19 @@ module Crichton
     describe '#include_alps_middleware' do
       context 'when configured' do
         it 'returns true when configured value is true' do
-          expect(configuration.include_alps_middleware).to be_true
+          expect(configuration.include_alps_middleware?).to be_true
         end
 
         it 'returns false when configured value is false' do
           @config = example_environment_config.merge({ 'include_alps_middleware' => false })
-          expect(configuration.include_alps_middleware).to be_false
+          expect(configuration.include_alps_middleware?).to be_false
         end
       end
 
       context 'when not configured' do
         it 'returns false' do
           @config = example_environment_config.except('include_alps_middleware')
-          expect(configuration.include_alps_middleware).to be_false
+          expect(configuration.include_alps_middleware?).to be_false
         end
       end
     end
