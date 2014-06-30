811e2ea4dbd10a59f36502bfe5c3918ff72f25f5
Chad Albers <calbers@mdsol.com>
2014-06-30 16:19:42 +0900

    added documentation

--- lib/crichton/discovery/entry_point.rb
+++ lib/crichton/discovery/entry_point.rb
@@ -25,13 +25,28 @@ module Crichton
         Addressable::URI.parse(File.join(Crichton.config.deployment_base_uri, @resource_uri_path)).to_s
       end
 
+      ##
+      #
+      # Returns the name of the resource type
+      #
+      # @return [String] name of the resource type
       def name
         @resource_name
       end
 
+      ##
+      #
+      # Returns the link relation type for the entry point, as a URL
+      # reference the alps document. Example; if the resource name is called
+      # 'studies' and supplied resource_id is "Studies" then this method
+      # uses Critchton.config.alps_base_uri to create the following URI:
+      # http://example.org/alps/Study#studies
+      #
+      # @return [String] URI of the link relation
       def link_relation
         Addressable::URI.parse(File.join(Crichton.config.alps_base_uri,"#{@resource_id}##{@resource_name}")).to_s
       end
+
       ##
       #
       # Equality operator for adding EntryPoints into a set collection
