f1456c4342aa53378a998b782e8509df02771d62
brutski <abrutski@mdsol.com>
2014-07-07 15:21:23 -0400

    fixing typo

--- lib/crichton/railtie.rb
+++ lib/crichton/railtie.rb
@@ -1,7 +1,7 @@
 require 'crichton/helpers'
 require 'crichton/middleware/registry_cleaner'
 require 'crichton/middleware/alps_profile_response'
-require 'crichton/middleware/service_resource_catalog'
+require 'crichton/middleware/service_resources_catalog'
 
 module Crichton
   class Railtie < Rails::Railtie
@@ -10,7 +10,7 @@ module Crichton
     initializer "crichton.insert_middleware" do |app|
       app.config.middleware.use "Crichton::Middleware::RegistryCleaner" if Rails.env.development?
       if config.use_discovery_middleware?
-        app.config.middleware.use "Crichton::Middleware::ServiceResourceCatalog", config.resources_catalog_response_expiry
+        app.config.middleware.use "Crichton::Middleware::ServiceResourcesCatalog", config.resources_catalog_response_expiry
       end
       if config.use_alps_middleware?
         app.config.middleware.use "Crichton::Middleware::AlpsProfileResponse", config.alps_profile_response_expiry
