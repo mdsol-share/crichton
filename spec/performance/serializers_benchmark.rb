7af8cfa1fb63884d1a762114b455926e803e03ad
Michael West <mwest@mdsol.com>
2014-07-21 08:39:31 -0400

    Update perf spec test

--- spec/performance/serializers_benchmark.rb
+++ spec/performance/serializers_benchmark.rb
@@ -11,13 +11,24 @@ describe Crichton::Discovery::EntryPoints do
   end
   let(:instance) {described_class.new([entry_point])}
 
+  ITERATIONS = 3
+  OP_COUNT = 2000
+
   describe '#to_media_type' do
     it 'is benchmarked' do
       [:hale_json, :hal_json, :json, :html, :xhtml].each do |media_type|
         puts "Benchmarching serializing '#{media_type}' media type:"
-        Benchmark.bm do |x|
-          x.report { 1000.times { instance.to_media_type(media_type) } }
+        results = Benchmark.bm do |x|
+          1.upto(ITERATIONS) do |i|
+            x.report { OP_COUNT.times { instance.to_media_type(media_type) } }
+          end
         end
+
+        average_total_times = results.map(&:total).inject(&:+) / ITERATIONS
+        average_operation_ms = (average_total_times * 1000) / OP_COUNT
+
+        puts "Invoked to_media_type #{OP_COUNT} times on a complex documents which took on average #{'%.4f' % average_total_times} seconds"
+        puts "It took #{'%.4f' % average_operation_ms} milliseconds total for each document"
         puts "====================================================="
       end
     end
