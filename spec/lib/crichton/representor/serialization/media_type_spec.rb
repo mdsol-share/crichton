7cb2051ad086a3c4de84c952f9450c2c13baad75
Aaron Weiner <aweiner@mdsol.com>
2014-11-07 11:47:41 -0500

    Unpending and update media type specs

--- spec/lib/crichton/representor/serialization/media_type_spec.rb
+++ spec/lib/crichton/representor/serialization/media_type_spec.rb
@@ -7,22 +7,23 @@ module Crichton
     module Serialization
       describe MediaType do
         let (:simple_test_instance) do
+          #TODO: We may want to put a real fixture here so we don't have to stub so much
           klass = Class.new do
             include MediaType
           end
           klass.new
         end
-        
+
         before do
           @options = double('options')
           @serializer = double('serializer')
-          stub_factory_method = allow(Crichton::Representor::Serializer).to receive(:build)
-          stub_factory_method.with(:media_type, simple_test_instance, @options).and_return(@serializer)
+          [Representors::Representor, RepresentorSerializer].each do |factory|
+            allow(factory).to receive(:new).with(any_args).and_return(@serializer)
+          end
         end
-        
+
         describe '#as_media_type' do
           it 'delegates to a built serializer for the media type' do
-            pending('is it still supposed to do this?')
             expect(@serializer).to receive(:as_media_type).with(@options)
             simple_test_instance.as_media_type(:media_type, @options)
           end
@@ -30,8 +31,8 @@ module Crichton
 
         describe '#to_media_type' do
           it 'delegates to a built serializer for the media type' do
-            pending('is it still supposed to do this?')
-            expect(@serializer).to receive(:to_media_type).with(@options)
+            expect(@serializer).to receive(:to_media_type).with(any_args, @options)
+            allow(simple_test_instance).to receive(:as_media_type).with(any_args)
             simple_test_instance.to_media_type(:media_type, @options)
           end
         end
