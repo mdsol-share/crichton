9203881ac3d86676dec456c1367ff91986ac4ef7
Shea Valentine <svalentine@mdsol.com>
2014-12-16 13:13:17 -0500

    removed superfluous code from options decorator

--- lib/crichton/descriptor/options_decorator.rb
+++ lib/crichton/descriptor/options_decorator.rb
@@ -12,43 +12,14 @@ module Crichton
       end
 
       def options
-        @decorated_options ||= if super && (external = super[EXTERNAL])
+        @decorated_options ||= if @descriptor_document && (external = @descriptor_document[EXTERNAL])
           source = external[SOURCE]
-          @target.respond_to?(source) ? respond_to_method(source, super) : super
+          @target.respond_to?(source) ? @target.send(source, @descriptor_document) : @descriptor_document
         else
-          super
+          @descriptor_document
         end
       end
 
-      private
-      def respond_to_method(method, options)
-        result = @target.send(method, options)
-        raise_if_invalid(result.is_a?(Hash), "#{method} method on target must return Hash object")
-
-        [EXTERNAL, LIST, HASH].each do |x|
-          if opts = result[x]
-            raise_if_invalid(conditions[x].call(opts))
-            return result
-          end
-        end
-        throw_response_error("#{result} is invalid response type.")
-      end
-
-      def conditions
-        {
-          EXTERNAL => ->(opts) { (opts[SOURCE] && opts[TARGET] && opts[PROMPT]) },
-          HASH => ->(opts) { opts.is_a?(Hash) },
-          LIST => ->(opts) { opts.is_a?(Array) }
-        }
-      end
-
-      def raise_if_invalid(condition, message = '')
-        throw_response_error(message) unless condition
-      end
-
-      def throw_response_error(message)
-        raise Crichton::TargetMethodResponseError, message
-      end
     end
   end
 end
