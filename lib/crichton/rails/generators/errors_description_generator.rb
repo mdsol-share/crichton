223acb445a4d47dcc66a9a00e5c2ce56cdad9997
Connor Savage <csavage@mdsol.com>
2014-12-22 15:36:27 -0500

    make errors name hyper errors, fix goofy singular assumption

--- lib/crichton/rails/generators/errors_description_generator.rb
+++ lib/crichton/rails/generators/errors_description_generator.rb
@@ -7,22 +7,22 @@ module Crichton
     source_root File.expand_path("../", __FILE__)
 
     argument :errors_class_path, type: :string, desc: "The path that the errors class will go to"
-    argument :resource_name, default: 'errors', type: :string, desc: "The name for the errors resource"
+    argument :resource_name, default: 'hyper_error', type: :string, desc: "The name for the hypermedia errors resource"
     argument :api_dir, type: :string, default: 'api_descriptors', desc: 'directory for our api docs'
 
     desc "This generator will generate a errors resource descriptor file and errors class."
     def create_description
       copy_file "errors_template.yaml", yaml_filename
       gsub_file yaml_filename, "{resource_name}", resource_name
-      gsub_file yaml_filename, "{resource_capitalize}", resource_name.capitalize
+      gsub_file yaml_filename, "{resource_capitalize}", resource_name.classify
       copy_file "error_class_template.txt", class_filename
       gsub_file class_filename, "{resource_name}", resource_name
-      gsub_file class_filename, "{resource_capitalize}", resource_name.capitalize
+      gsub_file class_filename, "{resource_capitalize}", resource_name.classify
     end
 
     private
     def yaml_filename
-       "#{api_dir}/#{resource_name}.yaml"
+       "#{api_dir}/#{resource_name}.yml"
     end
 
     def class_filename
@@ -30,4 +30,3 @@ module Crichton
     end
   end
 end
-
